acfe0c3234fb2a9645bf62f49f99319dbb3466fd
Sergio Acosta <sergioacostaquintana@gmail.com>
2026-01-27 22:59:53 +0000

    feat: Implement the complete Astro website structure including core pages, components, blog, and data management.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,22 @@
-import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
-import sitemap from "@astrojs/sitemap";
-
 import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), sitemap(), react()],
-  site: 'https://terenciosupermercados.com',
+  integrations: [
+    tailwind(), 
+    sitemap({
+      changefreq: 'weekly',
+      priority: 0.7,
+      lastmod: new Date()
+    }), 
+    react()
+  ],
+  site: 'https://terenciomarket.es',
+  compressHTML: true,
+  build: {
+    inlineStylesheets: 'auto',
+  }
 });
